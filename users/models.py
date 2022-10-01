91567fb9e0c45337ab986a26b4d12ebdd574c789
Toutchy kipik <toutchy.kipik@pm.me>
2022-10-01 04:40:18 +0200

    db

--- users/models.py
+++ users/models.py
@@ -1,6 +1,9 @@
+from django.contrib.auth import get_user_model
 from django.db import models
 from django.contrib.auth.models import User
 from PIL import Image
+
+user = get_user_model()
 
 
 # Extending User Model Using a One-To-One Link
